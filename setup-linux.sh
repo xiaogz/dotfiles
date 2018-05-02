18e2d882a18a992e24fc56dffb7c13ad687e7dc0
g3xiao <g3xiao@nettop1.student.cs.uwaterloo.ca>
2018-05-02 10:06:02 -0400

    Automate sourcing and remove file warning

--- setup-linux.sh
+++ setup-linux.sh
@@ -17,6 +17,7 @@ cd $curr_dir
 
 for file in $files; do
   echo acting on $file
+  touch $file
   mv ~/.$file $old_dir
   ln -fs $curr_dir/$file ~/.$file
 done
@@ -32,12 +33,17 @@ fi
 # add extra aliases to ~/.bashrc
 echo "# extra git-related aliases" >> ~/.bashrc
 echo "alias gs='git status'" >> ~/.bashrc
+echo "alias gsb='git status -sb'" >> ~/.bashrc
 echo "alias gl='git log'" >> ~/.bashrc
 echo "alias gd='git diff'" >> ~/.bashrc
 echo "alias glo='git log --pretty=oneline'" >> ~/.bashrc
+
+source ~/.bashrc
 
 # add extra settings to ~/.profile
 echo "# prevent ctrl+D from closing terminal" >> ~/.profile
 echo "set -o ignoreeof" >> ~/.profile
 echo "# change capslock to control" >> ~/.profile
 echo "setxkbmap -layout us -option ctrl:nocaps" >> ~/.profile
+
+source ~/.profile
